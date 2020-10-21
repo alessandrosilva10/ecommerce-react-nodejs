169602accdb50dd4024f5812ad620a2dc262d2f9
Alessandro <alessandro.mpgh@gmail.com>
2020-10-20 23:49:38 -0300

    added cors

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -16,14 +16,21 @@ const {
     create,
     read,
     remove,
-    update
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch
 } = require('../controllers/product');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', create, isAuth, isAdmin, requireSignin);
 router.delete('/product/:productId/:userId', remove, isAuth, isAdmin, requireSignin);
 router.put('/product/:productId/:userId', update, isAuth, isAdmin, requireSignin);
-
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post("/products/by/search", listBySearch);
 router.param('userId', userById);
 router.param('productId', productById);
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,8 @@ const {
 
 const { 
     userById,
+    read,
+    update,
 } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
@@ -17,6 +19,8 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     });
 });
 
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
 router.param('userId', userById)
 
 module.exports = router;
